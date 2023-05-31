4190ef3035d458624dda2d01c2f52509b3a759ee
David Martinez <123524976+Davidooj@users.noreply.github.com>
2023-05-31 12:01:58 +1000

    Delete Intro To Pandas directory

--- Intro To Pandas/dolladolla.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("pokemon_data.csv")
-
-with pd.option_context("display.max_rows", None, "display.max_columns", None):
-    print(df.head(15))
-    print(df.columns)
-
